075684b8c7b6fb2a3ffb2f402f3a4d5448816bcd
M.Kh!z@r <85388241+khizarbinmaalik@users.noreply.github.com>
2025-07-25 11:33:47 +0000

    init

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    domains: ["nextjs.org", "example.com"], // Add your allowed image domains here  
+  }
+};
+
+export default nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="container mx-auto p-4 bg-gray-900 flex flex-col items-center justify-center min-h-screen ">
+      <h1> Hellow World</h1>
+      <p>Welcome to my Next.js application!</p>
+    </div>
+  );
+}
