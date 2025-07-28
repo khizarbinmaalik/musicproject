a253082dc793a68c09d2dc056f8af0c691d0004c
M.Kh!z@r <85388241+khizarbinmaalik@users.noreply.github.com>
2025-07-28 11:15:17 +0000

    Completed the Home Page

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import { Spotlight } from "./ui/Spotlight";
 import { Button } from "./ui/moving-border";
-
+import { BackgroundLines } from "@/components/ui/background-lines";
 const HeroSection = () => {
   return (
+    <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
     <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
       <Spotlight
         className="-top-40 left-0 md:-top-20 md:left-60"
@@ -30,6 +31,7 @@ const HeroSection = () => {
         </div>
       </div>
     </div>
+    </BackgroundLines>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   darkMode: "class",
   content: [
@@ -10,11 +11,18 @@ module.exports = {
     extend: {
       animation: {
         spotlight: "spotlight 2s ease .75s 1 forwards",
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
       },
       keyframes: {
         spotlight: {
           "0%": { opacity: 0, transform: "translate(-72%, -62%) scale(0.5)" },
           "100%": { opacity: 1, transform: "translate(-50%,-40%) scale(1)" },
+        },
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
         },
       },
     },
