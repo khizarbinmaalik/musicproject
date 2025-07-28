cf8894c2ee030fa170b5e68da1bbe5e91ec9e1e3
M.Kh!z@r <85388241+khizarbinmaalik@users.noreply.github.com>
2025-07-28 11:48:28 +0000

    Completed the Contact Page, Now the Project is completed by by :)

--- src/components/TestimonialCards.tsx
+++ src/components/TestimonialCards.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
+import { Boxes } from "./ui/background-boxes";
 
 const TestimonialCards = () => {
   const musicSchoolTestimonials = [
@@ -38,6 +39,7 @@ const TestimonialCards = () => {
 
   return (
     <div className="h-[40rem] w-full dark:bg-black dark:bg-grid-white/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
+      <Boxes/>
       <h2 className="text-3xl font-bold text-center mb-8 z-10 text-white">
         Hear our Harmony: Voices of success
       </h2>
